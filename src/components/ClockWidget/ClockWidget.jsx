824d95fb8fa7b07b8e08a586d1ceb84858911918
KarolisGaiv <kgaivenis@gmail.com>
2022-03-11 11:36:28 +0200

    Add greeting funcionality

--- src/components/ClockWidget/ClockWidget.jsx
+++ src/components/ClockWidget/ClockWidget.jsx
@@ -4,10 +4,12 @@ import React, { useEffect, useState } from 'react';
 
 function ClockWidget() {
   const [date, setDate] = useState({});
+  const [greeting, setGreeting] = useState('Good afternoon');
 
   useEffect(() => {
     fetchTime();
-  }, []);
+    getGreeting();
+  }, [date]);
 
   const fetchTime = async () => {
     const response = await fetch('http://worldtimeapi.org/api/ip');
@@ -19,13 +21,22 @@ function ClockWidget() {
       minutes: ('0' + current_date.getMinutes()).slice(-2),
       abbreviation: data.abbreviation,
     };
-
     setDate(dateObj);
   };
 
+  function getGreeting() {
+    if (date.hours >= 12 && date.hours < 18) {
+      setGreeting('Good afternoon');
+    } else if (date.hours >= 18 && date.hours < 5) {
+      setGreeting('Good evening');
+    } else {
+      setGreeting('Good morning');
+    }
+  }
+
   return (
     <div className='clock-container'>
-      <div className='clock-container__greeting'>Greeting placeholder</div>
+      <div className='clock-container__greeting'>{greeting}</div>
       <div className='clock-container__time'>
         <p className='clock-container__time__current-date'>{date.hours}:</p>
         <p className='clock-container__time__current-date'>{date.minutes}</p>
