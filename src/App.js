28be6f9703241d65501fcf769b6b013701683a85
KarolisGaiv <kgaivenis@gmail.com>
2022-03-21 18:08:27 +0200

    Change background image based on screen size

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames';
 import './styles/main.scss';
 import Quote from './components/Quote/Quote';
 import ClockWidget from './components/ClockWidget/ClockWidget';
 
 function App() {
-  const [screenSize, setScreenSize] = useState();
   const [dayPhase, setDayPhase] = useState();
   const backgroundClasses = classNames('background', {
     'background --day': dayPhase === 'day',
     'background --night': dayPhase === 'night',
   });
-
-  useEffect(() => {
-    getScreenSize();
-  }, []);
-
-  function getScreenSize() {
-    let size = window.screen.width;
-
-    if (size < 768) {
-      setScreenSize('phone');
-    } else if (size >= 768 && size < 1023) {
-      setScreenSize('tablet');
-    } else {
-      setScreenSize('desktop');
-    }
-  }
 
   return (
     <div className='content__wrapper'>
